32a3c6b170c29e1e25d9b66042e545286e20f31d
Aditya Ojha <aditya.ojha2820@gmail.com>
2025-02-13 20:03:04 -0500

    AI thumnail generation working now (still working on submit)

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -135,16 +135,21 @@ const createPodcast = () => {
           </div>
           <div className="flex flex-col pt-10">
             <GeneratePodcast
-             setAudioStorageId={setAudioStorageId}
-             setAudio={setAudioUrl}
-             voiceType={voiceType!}
-             audio={audioUrl}
-             voicePrompt={voicePrompt}
-             setVoicePrompt={setVoicePrompt}
-             setAudioDuration={setAudioDuration}
+              setAudioStorageId={setAudioStorageId}
+              setAudio={setAudioUrl}
+              voiceType={voiceType!}
+              audio={audioUrl}
+              voicePrompt={voicePrompt}
+              setVoicePrompt={setVoicePrompt}
+              setAudioDuration={setAudioDuration}
             />
 
             <GenerateThumbnail
+              setImage={setImageUrl}
+              setImageStorageId={setImageStorageId}
+              image={imageUrl}
+              imagePrompt={imagePrompt}
+              setImagePrompt={setImagePrompt}
             />
 
             <div className="mt-10 w-full">
--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lovely-flamingo-139.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "qualified-flamingo-492.convex.cloud",
+      },
     ],
   }, // <-- Closing brace for images was missing
 };
